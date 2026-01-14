e12c69d873713dc7b0f518d817710dfc863b5f0e
nahidmia98655 <nahidmiah0190@gmail.com>
2026-01-14 23:48:37 +0600

    Initial commit via NS AI Code Studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,19 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinKapt) apply false
-    alias(libs.plugins.hilt) // Hilt plugin is applied only in the app module
+    // Hilt plugin is applied only in the app module via version catalog
+    // (no need to apply it here, but we need its classpath for resolution)
+}
+
+// Explicitly add the Hilt Gradle plugin classpath so that the plugin can be resolved
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
 }
 
 // No additional configuration needed at the top level for this simple project.
